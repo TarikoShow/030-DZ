ee6ef904bd8570e57806f1496d9fe081518b5f16
Evgenyi Degtyarenko <163438276+TarikoShow@users.noreply.github.com>
2024-04-03 14:04:15 +0300

    Update HomeWork.js
    Теперь работает.
    Открывает сайт и находится кликает на ссылки на нем.

--- HomeWork.js
+++ HomeWork.js
@@ -1,20 +1,21 @@
 // ==UserScript==
-// @name         My New Bot
+// @name         DZ Bot
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Bot for Bing new
+// @description  Bot for Zapmeta
 // @author       Degtyarenko Evgeniy
 // @match        https://www.zapmeta.com/*
 // @grant        none
 // ==/UserScript==
 
 let input = document.getElementsByName("q")[0];
+let searchBtn = document.querySelector(".search-header__field-button");
 let links = document.links;
-let searchBtn = document.getElementsByName("search-header__field-button");
+let difference = document.querySelector(".jumbotron__logo-image");
 let keywards = ["Базовые вещи про GIT", "10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress"];
-let keyward = keywards[getRandom(0, keywards.lenght)];
+let keyward = keywards[getRandom(0, keywards.length)];
 
-if (searchBtn !== undefined) {
+if (difference !== null) {
   input.value = keyward;
   searchBtn.click();
 } else {
